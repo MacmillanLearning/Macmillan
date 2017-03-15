5998118606a39d888784b4c84f196eaeda83f793
K <474434@CTS.COM>
2017-03-15 13:04:19 +0530

    check in macmillantest

---
+++ custom/macmillan/macmillantest/gensrc/org/macmillan/test/constants/GeneratedMacmillanTestConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Mar 10, 2017 1:07:25 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.macmillan.test.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedMacmillanTestConstants
+{
+	public static final String EXTENSIONNAME = "macmillantest";
+	
+	protected GeneratedMacmillanTestConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
